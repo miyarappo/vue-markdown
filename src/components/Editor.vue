444cf918392b7d62852910e6de63c215c5481893
Minoru <miyarappo@gmail.com>
2018-08-13 22:46:34 +0900

    add login & logout func

---
+++ src/components/Editor.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="editor">
+        <h1>Editor画面</h1>
+        <span>{{ user.displayName }}</span>
+        <button @click="logout">ログアウト</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'editor',
+    props: ['user'],
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        logout() {
+            firebase.auth().signOut()
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
+
